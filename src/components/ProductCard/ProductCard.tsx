68bd88c0261d67be74e6e820560780fc13e16390
Aleksey Bogdanov <aleksey@bogdanov77.ru>
2026-02-21 14:58:04 +0300

    completed

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -8,10 +8,12 @@ interface ProductCardProps {
     currency_code: string;
     price: string;
     quantity: number;
+    onClick?: () => void;
 }
 
-export function ProductCard({ MainImage, title, currency_code, price, quantity}: ProductCardProps) {
+export function ProductCard({ url, MainImage, title, currency_code, price, quantity}: ProductCardProps) {
     let code: string;
+
     switch (currency_code) {
         case "USD":
             code = '$';
@@ -26,15 +28,24 @@ export function ProductCard({ MainImage, title, currency_code, price, quantity}:
             code = currency_code + ' ';
     }
 
+    let stockClass: string = 'stock-medium';
+    if (quantity <= 10) stockClass = 'stock-low';
+    if (quantity > 20) stockClass = 'stock-high';
+
+    const onClick: () => void = () => {
+        document.location=url
+    }
+
+
     return (
-        <div className="product-card">
+        <div className="product-card" onClick={onClick}>
             <img src={MainImage?.url_570xN}
                  alt="item.title" className="product-image"/>
             <div className="product-info">
                 <h3 className="product-title">{title}</h3>
                 <div className="price-container">
                     <div className="product-price">{code}{price}</div>
-                    <span className="stock-badge stock-medium">{quantity} left</span>
+                    <span className={"stock-badge " + stockClass}>{quantity} left</span>
                 </div>
             </div>
         </div>
